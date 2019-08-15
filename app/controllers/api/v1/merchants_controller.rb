d05e835f447a05d1051e3de8802d0b3b0cd60db1
Nathan Thomas <nathan.gordon.thomas@gmail.com>
2019-08-14 20:13:24 -0600

    Initial commit

---
+++ app/controllers/api/v1/merchants_controller.rb
@@ -0,0 +1,13 @@
+class Api::V1::MerchantsController < ApplicationController
+
+  def index
+    render json: Merchant.all
+    # render json: MerchantSerializer.new(Merchant.all)
+  end
+
+  def show
+    render json: Merchant.find(params[:id])
+  #render json: MerchantSerializer.new(Merchant.find(params[:id]))
+  end
+
+end
