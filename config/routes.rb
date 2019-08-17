0c34878119ba8f740de91d01c401f0aa9794b6eb
Nathan Thomas <nathan.gordon.thomas@gmail.com>
2019-08-17 16:55:59 -0600

    Add controller and endpoints for favorite_merchant

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+
   namespace :api do
     namespace :v1 do
       namespace :merchants do
@@ -18,6 +19,23 @@ Rails.application.routes.draw do
       # get '/merchants/:id', to: 'merchants#show'
     end
   end
+
+  # namespace :api do
+  #   namespace :v1 do
+  #     resources :customers, only: [:show]
+  #   end
+  # end
+
+   get '/api/v1/customers/:id/favorite_merchant', to: 'api/v1/customers/favorite_merchant#show'
+   #returns a merchant where the customer has conducted the most successful transactions
+
+
+
+
+
+
+
+
   # get '/api/v1/merchants/find?parameters, to: search#index'
   # get '/api/v1/merchants/find_all?parameters, to: search#show'
 
--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -8,5 +8,6 @@ RSpec.describe Invoice, type: :model do
 
     it {should have_many :invoice_items}
     it {should have_many(:items).through(:invoice_items)}
+    it {should have_many :transactions}
   end
 end
